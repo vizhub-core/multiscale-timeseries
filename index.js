a60b56a89fb0f56769878aecdfa61c2150ffbbe5
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-05 10:26:14 -0500

    Add weeks

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const { timeFormat } = require('d3-time-format');
 
-const initialize = () => ({
-  days: {},
-});
+const initialize = () => ({});
 
+//const getMonth = timeFormat('%Y-%m-%dT%H:%M');
 const getDay = timeFormat('%Y-%m-%d');
-//const getDay = timeFormat('%Y-%m-%dT%H:%M');
+const getWeek = timeFormat('%Y-W%V');
+const getMonth = timeFormat('%Y-%m');
+const getYear = timeFormat('%Y');
 
-const increment = (record, date) => {
+const increment = (record = {}, date) => {
   const day = getDay(date);
+  const week = getWeek(date);
   return {
-    days: { ...record.days, [day]: (record.days[day] || 0) + 1 },
+    days: {
+      ...record.days,
+      [day]: ((record.days && record.days[day]) || 0) + 1,
+    },
+    weeks: {
+      ...record.weeks,
+      [week]: ((record.weeks && record.weeks[week]) || 0) + 1,
+    },
   };
 };
 
--- test.js
+++ test.js
@@ -2,19 +2,20 @@ const assert = require('assert');
 const { initialize, increment } = require('./index');
 
 describe('Multiscale Timeseries', () => {
-  describe('initialize()', () => {
-    it('should initialize a timeseries record', () => {
-      assert.deepEqual(initialize(), { days: {} });
-    });
-  });
   describe('increment()', () => {
-    let record = initialize();
+    let record;
     const date = new Date('2020-10-05T14:32:40.441Z');
     it('should increment a timeseries record', () => {
       record = increment(record, date);
-      assert.deepEqual(record, { days: { '2020-10-05': 1 } });
+      assert.deepEqual(record, {
+        days: { '2020-10-05': 1 },
+        weeks: { '2020-W41': 1 },
+      });
       record = increment(record, date);
-      assert.deepEqual(record, { days: { '2020-10-05': 2 } });
+      assert.deepEqual(record, {
+        days: { '2020-10-05': 2 },
+        weeks: { '2020-W41': 2 },
+      });
     });
   });
 });
