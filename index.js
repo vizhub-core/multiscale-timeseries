fc8abc28e95d4fbeacb6ce72a29da4f829116d51
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-05 10:38:18 -0500

    Add all desired intervals

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const { timeFormat } = require('d3-time-format');
 
-const initialize = () => ({});
-
-//const getMonth = timeFormat('%Y-%m-%dT%H:%M');
-const getDay = timeFormat('%Y-%m-%d');
-const getWeek = timeFormat('%Y-W%V');
-const getMonth = timeFormat('%Y-%m');
-const getYear = timeFormat('%Y');
-
 const intervals = [
+  { property: 'minutes', format: timeFormat('%Y-%m-%dT%H:%M') },
+  { property: 'hours', format: timeFormat('%Y-%m-%dT%H') },
   { property: 'days', format: timeFormat('%Y-%m-%d') },
   { property: 'weeks', format: timeFormat('%Y-W%V') },
+  { property: 'months', format: timeFormat('%Y-%m') },
+  { property: 'quarters', format: timeFormat('%Y-Q%q') },
+  { property: 'years', format: timeFormat('%Y') },
 ];
 
 const increment = (record = {}, date) => {
@@ -25,4 +22,5 @@ const increment = (record = {}, date) => {
     };
   }, record);
 };
-module.exports = { initialize, increment };
+
+module.exports = { increment };
--- test.js
+++ test.js
@@ -8,13 +8,23 @@ describe('Multiscale Timeseries', () => {
     it('should increment a timeseries record', () => {
       record = increment(record, date);
       assert.deepEqual(record, {
+        minutes: { '2020-10-05T10:32': 1 },
+        hours: { '2020-10-05T10': 1 },
         days: { '2020-10-05': 1 },
         weeks: { '2020-W41': 1 },
+        months: { '2020-10': 1 },
+        quarters: { '2020-Q4': 1 },
+        years: { 2020: 1 },
       });
       record = increment(record, date);
       assert.deepEqual(record, {
+        minutes: { '2020-10-05T10:32': 2 },
+        hours: { '2020-10-05T10': 2 },
         days: { '2020-10-05': 2 },
         weeks: { '2020-W41': 2 },
+        months: { '2020-10': 2 },
+        quarters: { '2020-Q4': 2 },
+        years: { 2020: 2 },
       });
     });
   });
