efe759b98b6af405b85217b8add699c1c95229b9
Curran Kelleher <curran.kelleher@gmail.com>
2020-12-05 10:33:01 -0500

    Refactor

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ const getWeek = timeFormat('%Y-W%V');
 const getMonth = timeFormat('%Y-%m');
 const getYear = timeFormat('%Y');
 
-const increment = (record = {}, date) => {
-  const day = getDay(date);
-  const week = getWeek(date);
-  return {
-    days: {
-      ...record.days,
-      [day]: ((record.days && record.days[day]) || 0) + 1,
-    },
-    weeks: {
-      ...record.weeks,
-      [week]: ((record.weeks && record.weeks[week]) || 0) + 1,
-    },
-  };
-};
+const intervals = [
+  { property: 'days', format: timeFormat('%Y-%m-%d') },
+  { property: 'weeks', format: timeFormat('%Y-W%V') },
+];
 
+const increment = (record = {}, date) => {
+  return intervals.reduce((accumulator, { property, format }) => {
+    const key = format(date);
+    return {
+      ...accumulator,
+      [property]: {
+        ...record[property],
+        [key]: ((record[property] && record[property][key]) || 0) + 1,
+      },
+    };
+  }, record);
+};
 module.exports = { initialize, increment };
